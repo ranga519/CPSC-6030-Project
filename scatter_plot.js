1c44b0e810f066a05ff8caaffd15227b081bc52d
Ranga Baminiwatte <64362605+ranga519@users.noreply.github.com>
2023-11-15 13:25:31 -0500

    Add files via upload

--- scatter_plot.js
+++ scatter_plot.js
@@ -16,8 +16,10 @@ d3.csv("1970-2021_DISASTERS_UPDATED_COUNTRIES.csv").then(function(dataset) {
     var data = dataset.map(function(d) {
         return {
             Year: +d.Year,
+            Month: +d.Start_Month,
             Total_Affected: +d.Total_Affected,
-            Country: d.Country
+            Country: d.Country,
+            Disaster_Type: d.Disaster_Type
         };
     });
 
@@ -25,15 +27,13 @@ d3.csv("1970-2021_DISASTERS_UPDATED_COUNTRIES.csv").then(function(dataset) {
     var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
     // Create an SVG element to contain the scatter plot
-    
     var svg = d3.select("#scatterplot")
         .attr("width", dimensions.width)
         .attr("height", dimensions.height);
-    var container = document.getElementById("q2-container")
-    // Create x-scale mapping the 'Year' values to the horizontal space
-    var xScale = d3.scaleLinear()
-        .domain([d3.min(data, function(d) { return d.Year; }), d3.max(data, function(d) { return d.Year; })])
-        .nice()
+
+    // Create x-scale mapping both 'Year' and 'Month' to the horizontal space
+    var xScale = d3.scaleTime()
+        .domain([d3.min(data, function(d) { return new Date(d.Year, d.Month - 1, 1); }), d3.max(data, function(d) { return new Date(d.Year, d.Month - 1, 1); })])
         .range([dimensions.margin.left, dimensions.width - dimensions.margin.right]);
 
     // Create y-scale mapping the 'Total_Affected' values to the vertical space using a logarithmic scale
@@ -45,20 +45,47 @@ d3.csv("1970-2021_DISASTERS_UPDATED_COUNTRIES.csv").then(function(dataset) {
     // Create y-axis with appropriate tick values using a logarithmic scale and custom tick format
     var yAxis = d3.axisLeft(yScale).ticks(5, "~s").tickFormat(d3.format(".2s"));
 
+    // Create a tooltip div
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
     // Create circles for each data point, assigning colors based on 'Country'
     var dots = svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function(d) { return xScale(d.Year); }) // X-coordinate based on 'Year'
+        .attr("cx", function(d) { return xScale(new Date(d.Year, d.Month - 1, 1)); }) // X-coordinate based on both 'Year' and 'Month'
         .attr("cy", function(d) { return yScale(Math.max(1, d.Total_Affected)); }) // Y-coordinate based on 'Total_Affected', ensuring a minimum value of 1
         .attr("r", 3) // Radius of the circles
-        .attr("fill", function(d) { return colorScale(d.Country); }); // Assign color based on 'Country'
+        .attr("fill", function(d) { return colorScale(d.Country); }) // Assign color based on 'Country'
+        .on("mouseover", function(d) {
+            // Show tooltip on mouseover
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0.9);
+            tooltip.html(
+                "Country: " + d.Country + "<br>" +
+                "Disaster Type: " + d.Disaster_Type + "<br>" +
+                "Total Affected: " + d.Total_Affected
+            )
+                .style("left", (d3.event.pageX + 5) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            // Hide tooltip on mouseout
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
     // Add x-axis with appropriate tick values
     svg.append("g")
         .attr("transform", "translate(0," + (dimensions.height - dimensions.margin.bottom) + ")")
-        .call(d3.axisBottom(xScale).tickFormat(d3.format("d"))); // Use 'd3.format("d")' to format as integers
+        .call(d3.axisBottom(xScale)
+            .ticks(d3.timeYear.every(5)) // Show ticks every 5 years
+            .tickFormat(d3.timeFormat("%Y")) // Format ticks as years
+        );
 
     // Add y-axis with appropriate tick values using a logarithmic scale
     svg.append("g")
